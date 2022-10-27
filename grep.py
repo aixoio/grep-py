d4052e881bc07a8ec8a29d9ac3cff7ea6e6c6a9b
aixoio <aixoio@yahoo.com>
2022-10-27 17:49:55 +1100

    Remove Mode 3 and add submode 2 to mode 2

--- grep.py
+++ grep.py
@@ -81,11 +81,56 @@ def folderMode(submode):
         
         print("In all of the files above we have found your query")
 
-    
-    
-def rootMode(submode):
-    
-    pass
+    if submode == "2":
+        
+        filecontents = []
+
+        for filepath in files:
+            
+            truename = filepath[2::]
+            
+            with open(truename, "r") as filecontent:
+                
+                contents = filecontent.read()
+                
+                filecontents.append({
+                    
+                    "name": filepath,
+                    "content": contents
+                    
+                })
+                
+        findThat = input("Enter what you want to find: ")
+        replaceIt = input("Enter what you want to replace it with: ")
+        
+        finds = []
+        
+        for filedata in filecontents:
+            
+            if filedata.get("content").find(findThat) > -1:
+                
+                with open(filedata.get("name"), "w") as cfile:
+                    
+                    newfile = filedata.get("content").replace(findThat, replaceIt)
+                    
+                    cfile.write(newfile)
+                    
+                    
+
+                finds.append(filedata.get("name"))
+                
+        print()
+            
+        
+        for find in finds:
+            
+            print(find[2::])
+        
+        
+        print()
+        
+        print("In all of the files above we have found your query")
+        
 
     
 
@@ -94,7 +139,6 @@ print()
 print("MODES")
 print("1. File")
 print("2. Folder")
-print("3. Root")
 
 mode = input("Enter a mode: ")
 
@@ -122,8 +166,5 @@ if mode == "2":
     
     folderMode(submode)
     
-    
-if mode == "3":
-    
-    rootMode(submode)
+
     
